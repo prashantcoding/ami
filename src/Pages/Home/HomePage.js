a5055a48cf8e98d8d051fb4ee15787a28ecadc9b
prashantcoding <soultaker399@gmail.com>
2022-01-18 12:24:02 +0530

    respo

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -4,56 +4,65 @@ import ami from "./ami.png";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import Sidebar from "../../Component/Sidebar/index";
-import {FaHamburger} from 'react-icons/fa';
-
+import { FaHamburger } from "react-icons/fa";
+import sample from './bg7.mp4';
 const HomePage = (props) => {
   const [isOpen, setisOpen] = useState(false);
   const toggle = () => {
     setisOpen(!isOpen);
   };
   return (
-    <motion.div
-      initial={{ y: -2000 }}
-      animate={{ y: 0 }}
-      exit={{ y: -2000 }}
-      transition={{ duration: 1 }}
-    >
-      <Sidebar isOpen={isOpen} toggle={toggle} />
+    <>
+      <div>
+        <video className={styles.videoTag} autoPlay loop muted >
+          <source src={sample} type="video/mp4" />
+        </video>
+      </div>
       <div className={styles.nav}>
-        <div className={styles.logo}>
-          <img src={ami} />
-          <h1>Amichroma</h1>
-        </div>
+          <div className={styles.logo}>
+            <img src={ami} />
+            <h1>Amichroma</h1>
+          </div>
 
-        <div className={styles.dig_line}></div>
+          <div className={styles.dig_line}></div>
 
-        <div className={styles.content}>
-          <span className={styles.link}>Workshops</span>
-          <Link to="/events" className={styles.link}>
-            Events
-          </Link>
-          <span className={styles.link}>Gallery</span>
-          <span className={styles.link}>About us </span>
+          <div className={styles.content}>
+            <span className={styles.link}>Workshops</span>
+            <Link to="/events" className={styles.link}>
+              Events
+            </Link>
+            <span className={styles.link}>Gallery</span>
+            <span className={styles.link}>About us </span>
+          </div>
+          <div className={styles.dig_line_two}></div>
+          <div className={styles.signup}>SignUp</div>
+          <div onClick={toggle} className={styles.ham}>
+            <FaHamburger size={40}></FaHamburger>
+          </div>
         </div>
-        <div className={styles.dig_line_two}></div>
-        <div className={styles.signup}>SignUp</div>
-        <div onClick={toggle} className={styles.ham}>
-         
-          <FaHamburger size={40}></FaHamburger>
+      <motion.div
+        initial={{ y: -2000 }}
+        animate={{ y: 0 }}
+        exit={{ y: -2000 }}
+        transition={{ duration: 1 }}
+      >
+        <Sidebar isOpen={isOpen} toggle={toggle} />
+        
+        <div className={styles.box_main}>
+          <div className={styles.box_memory}>
+            <center className={styles.slogan}>Memories</center>
+          </div>
+          <div className={styles.box_events}>
+            <center span className={styles.slogan}>
+              Events
+            </center>
+          </div>
+          <div className={styles.box}>
+            <center className={styles.slogan}>Workshops</center>
+          </div>
         </div>
-      </div>
-      <div className={styles.box_main}>
-        <div className={styles.box_memory}>
-          <center className={styles.slogan}>Memories</center>
-        </div>
-        <div className={styles.box_events}>
-          <center span className={styles.slogan}>Events</center>
-        </div>
-        <div className={styles.box}>
-          <center className={styles.slogan}>Workshops</center>
-        </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </>
   );
 };
 
